c6b238db760de7609ef918e18f9881be54b718bf
Milkyzor <ricardo.rocheta@icloud.com>
2023-12-30 00:04:34 +0000

    General idea for program finished

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+// PLAN:
+
+1 - Fictional Facts Generator
+
+2 - Facts will be based on When? What? Why? And how it happened?
+
+3 - Include comments in your code for clarity.
+
+4 - Create and array of strings for each category
+
+5 - Develop function to randomly select each component of the message.
+
+6 - Create a function to combine these components into a complete message.
+
+7 - Ensure the syntax is correct and the program runs without errors.
+
+8 - Run your program multiple times and check for varied and random outputs.
+
+9 - Use Git commands to track changes (git add, git commit) and regularly push updates to GitHub.
+
+10 - Refactor code for efficiency and readability.
+
+11 - Write a README file explaining your program, how to run it, and its features.
+
+12 - Do a final round of testing.
+*/
